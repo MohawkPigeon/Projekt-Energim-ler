7b4618fbdf63c013f8318d22cbd99d974418b0ec
MohawkPigeon <42830998+MohawkPigeon@users.noreply.github.com>
2018-12-10 13:37:15 +0100

    1

--- src/Meter.java
+++ src/Meter.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 
 public class Meter {
     private ArrayList<Reading> readings=new ArrayList();
+    private ArrayList<Meter> meterList=new ArrayList();
     private String measureType;
     private int nr;
 
@@ -33,6 +34,11 @@ public class Meter {
     public void addReading(int value, String type, LocalDate date){
         Reading reading = new Reading(value, type, date);
         readings.add(reading);
+    }
+
+    public void addMeter(ArrayList<Reading> readings, String measureType, int nr){
+        Meter meter=new Meter(readings, measureType, nr);
+        meterList.add(meter);
     }
 
 
